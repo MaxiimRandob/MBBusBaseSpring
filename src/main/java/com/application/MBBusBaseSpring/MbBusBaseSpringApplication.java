ea5c249856a88996d05dc02178a38144f5be4b7f
MaxiimRandob <madrandob@gmail.com>
2020-06-21 16:37:06 +0300

    basic runnable application

---
+++ src/main/java/com/application/MBBusBaseSpring/MbBusBaseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.application.MBBusBaseSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbBusBaseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbBusBaseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/MBBusBaseSpring/MbBusBaseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.MBBusBaseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbBusBaseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
