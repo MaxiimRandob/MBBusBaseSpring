308541078a5918599d22ef51ee8d5025310bfe98
MaxiimRandob <madrandob@gmail.com>
2020-06-25 14:19:01 +0300

    Runnable home page with correct file structure

--- src/main/java/com/application/MBBusBaseSpring/controller/HomeController.java
+++ src/main/java/com/application/MBBusBaseSpring/controller/HomeController.java
@@ -1,7 +1,17 @@
 package com.application.MBBusBaseSpring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
+
+    @GetMapping
+    public String home()
+    {
+        return "home";
+    }
 }
