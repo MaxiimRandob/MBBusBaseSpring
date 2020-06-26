f43e842dcf06ddd690a736db30e063f3ceb8af1c
MaxiimRandob <madrandob@gmail.com>
2020-06-26 11:48:50 +0300

    Entity classes first declaration. User case only

---
+++ src/main/java/com/application/MBBusBaseSpring/entity/Admin.java
@@ -0,0 +1,8 @@
+package com.application.MBBusBaseSpring.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("1")
+public class Admin { }
---
+++ src/main/java/com/application/MBBusBaseSpring/entity/User.java
@@ -0,0 +1,99 @@
+package com.application.MBBusBaseSpring.entity;
+
+
+import javax.persistence.*;
+
+@Entity(name = "User")
+@Table(name = "user")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "Discriminator", discriminatorType = DiscriminatorType.INTEGER)
+public class User {
+    @Id
+    private int idUser;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "Bus_idBus")
+    @OneToOne
+    private String busId;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBusId() {
+        return busId;
+    }
+
+    public void setBusId(String busId) {
+        this.busId = busId;
+    }
+}
