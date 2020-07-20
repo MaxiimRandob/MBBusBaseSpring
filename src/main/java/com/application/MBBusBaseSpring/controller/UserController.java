c8b027d2e1c9c8443b08b34eac105f4f6c73c350
MaxiimRandob <madrandob@gmail.com>
2020-07-20 11:48:44 +0300

    Small changes into ddl.sql and bug fixes

--- src/main/java/com/application/MBBusBaseSpring/controller/UserController.java
+++ src/main/java/com/application/MBBusBaseSpring/controller/UserController.java
@@ -1,26 +1,14 @@
 package com.application.MBBusBaseSpring.controller;
 
-import com.application.MBBusBaseSpring.dao.UserRepository;
-import com.application.MBBusBaseSpring.entity.Admin;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-
-    @Autowired
-    UserRepository repository;
-
-    @GetMapping()
-    public String profile(@PathVariable int id, Model model)
-    {
-        Admin admin = (Admin) repository.findById(id).get();
-        model.addAttribute("admin", admin);
-        return "adminProfile";
+    @GetMapping
+    public String profile() {
+        return "redirect:/driver";
     }
 }
